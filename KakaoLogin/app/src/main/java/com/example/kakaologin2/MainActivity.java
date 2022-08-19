1e43a95ec49f543e1c0bb7698d27478be4838be2
kche1106 <kche011106@gmail.com>
2022-08-19 13:26:56 +0900

    KakaoLogin

---
+++ KakaoLogin/app/src/main/java/com/example/kakaologin2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.kakaologin2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    String strNickname, strProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvNickname = findViewById(R.id.tvNickname);
+        TextView tvProfile = findViewById(R.id.tvProfile);
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+        Intent intent = getIntent();
+        strNickname = intent.getStringExtra("name");
+        strProfile = intent.getStringExtra("profile");
+
+        tvNickname.setText(strNickname);
+        tvProfile.setText(strProfile);
+
+        btnLogout.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "정상적으로 로그아웃되었습니다.", Toast.LENGTH_SHORT).show();
+
+                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
+                    @Override
+                    public void onCompleteLogout() {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+}
