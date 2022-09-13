09726ccce1b717a5dc91620bd1858eb57fb2f21b
kche1106 <kche011106@gmail.com>
2022-09-13 17:35:54 +0900

    SQLite get Data

--- Basket/app/src/main/java/com/example/basket/MainActivity.java
+++ Basket/app/src/main/java/com/example/basket/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.basket;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,8 +15,6 @@ public class MainActivity extends AppCompatActivity
 {
     TextView device, barcodeId, barcodeType, amount;
     Button add_button, show_button;
-    SQLiteDatabase sqlDB;
-    EditText edtBarcodeType, edtBarcodeId, edtAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -47,32 +46,9 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
 
-                DBHelper dbHelper = new DBHelper(MainActivity.this);
-                sqlDB = dbHelper.getReadableDatabase();
-                Cursor cursor;
-                cursor = sqlDB.rawQuery("SELECT * FROM basket;", null);
+                Intent intent = new Intent(getApplicationContext(), BasketActivity.class);
+                startActivity(intent);
 
-                edtBarcodeType = findViewById(R.id.edtBarcodeType);
-                edtBarcodeId = findViewById(R.id.edtBarcodeId);
-                edtAmount = findViewById(R.id.edtAmount);
-
-                String barcodeTypeStr =  "바코드 타입" + "\r\n" + "-------" + "\r\n";;
-                String barcodeIdStr =  "바코드 번호" + "\r\n" + "-------" + "\r\n";;
-                String amountStr =  "개수" + "\r\n" + "-------" + "\r\n";;
-
-                while(cursor.moveToNext()){
-                    barcodeTypeStr += cursor.getString(2) + "\r\n";
-                    barcodeIdStr += cursor.getString(3) + "\r\n";
-                    amountStr += cursor.getString(4) + "\r\n";
-                }
-
-                //출력해주기
-                edtBarcodeType.setText(barcodeTypeStr);
-                edtBarcodeId.setText(barcodeIdStr);
-                edtAmount.setText(amountStr);
-
-                cursor.close();
-                sqlDB.close();
             }
         });
     }
